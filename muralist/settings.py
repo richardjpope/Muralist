3b259b5e047a1ae83badc70967829d0b52d3cbd1
Richard <richard@memespring.co.uk>
2010-05-16 11:04:00 +0100

    test deploy

--- muralist/settings.py
+++ muralist/settings.py
@@ -10,9 +10,9 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'decision'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_NAME = 'muralist_live'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'muralist_live'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'Xkb1s0Yf38'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
