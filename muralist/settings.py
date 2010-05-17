518bf40de38578f54f66e2573f4a6b1f9e5aeda0
Richard <richard@memespring.co.uk>
2010-05-17 11:53:21 +0100

    settings

--- muralist/settings.py
+++ muralist/settings.py
@@ -1,20 +1,5 @@
 # Django settings for web project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'muralist_live'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'muralist_live'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'Xkb1s0Yf38'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -33,10 +18,6 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/data/vhost/decision/web/media'
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
@@ -46,9 +27,6 @@ MEDIA_URL = 'media/'
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/admin-media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '8g+j&^@ad*sg1zopo2zv7ba8&10sn_=qxl@9c2og@uv16l4nqn'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -76,5 +54,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'south',
-    'muralist.murals',    
+    'murals',    
 )
+
+try:
+    from local_settings import *
+except ImportError:
+    print "Missing local_settings.py file or file not in PATH"
