07997d14965414b2616d3b3e1c1ea01a3a142bb6
Richard <richard@memespring.co.uk>
2010-05-17 17:13:24 +0100

    media

--- muralist/urls.py
+++ muralist/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', muralist_views.index, name="index"),
     (r'^admin/', include(admin.site.urls)),    
+    (r'^admin/(.*)', admin.site.root),
     
     # static media server for the dev sites / local dev
     url(r'^media/(?P<path>.*)$',       'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes':True}),    
