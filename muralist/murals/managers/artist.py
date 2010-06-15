e92de092dc9a660d2f3f80691e9b8279781f00eb
Richard <richard@memespring.co.uk>
2010-06-15 17:40:33 +0100

    about link

--- muralist/murals/managers/artist.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class ArtistManager(models.Manager):
-
-    def get_query_set(self):
-        return super(ArtistManager, self).get_query_set().filter(published=True)
--- muralist/murals/managers/mural.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class MuralManager(models.Manager):
-
-    def get_query_set(self):
-        return super(MuralManager, self).get_query_set().filter(published=True)
