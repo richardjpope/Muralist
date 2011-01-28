235c91f592ebb5d8c71fecaae0881c82bddee28f
Richard <richard@memespring.co.uk>
2011-01-28 17:28:45 +0000

    bug on volunteer page

--- muralist/volunteer_tasks/views.py
+++ muralist/volunteer_tasks/views.py
@@ -5,6 +5,6 @@ import settings
 
 def volunteer (request):
 
-    new_tasks = models.Task.objects.filter(completed=True)
+    new_tasks = models.Task.objects.filter(completed=False)
     completed_tasks = models.Task.objects.filter(completed=True)
     return render_to_response('tasks.html', {'new_tasks': new_tasks, 'completed_tasks': completed_tasks,}, context_instance = RequestContext(request))
